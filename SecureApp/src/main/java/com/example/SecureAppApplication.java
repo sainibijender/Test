c2c79630845b04aff8c2ff65a3b92ebacfb1d12d
sainibijender <67185699+sainibijender@users.noreply.github.com>
2020-06-20 19:33:43 +0530

    Add files via upload

---
+++ SecureApp/src/main/java/com/example/SecureAppApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAppApplication.class, args);
+	}
+
+}
+
